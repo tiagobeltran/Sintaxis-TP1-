3435ba333bdf64728274f3d2ea6e06e72e051034
Maia Velazquez <maia.velazquez@belltech.la>
2025-06-16 23:08:06 -0300

    funciones limpiarEspacios, derivarGramatica y buscarNoTerminal

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <time.h>
@@ -23,6 +24,60 @@ struct Gramatica
     int cant_producciones;
 };
 
+// Devuelve el primer no terminal en una cadena de derivacion
+char buscarNoTerminal(char *cadena, struct Gramatica *G) {
+    for (int i = 0; cadena[i] != '\0'; i++) {
+        for (int j = 0; G->noTerminales[j] != '\0'; j++) {
+            if (cadena[i] == G->noTerminales[j])
+                return cadena[i];
+        }
+    }
+    return '\0';
+}
+
+// Proceso de derivación
+void derivarGramatica (struct Gramatica *G)
+{
+    char cadena[2048] = "";
+    cadena[0] = G->axioma;
+    cadena[1] = '\0';
+
+    srand(time(NULL));
+    
+    char NTActual;
+     while ((NTActual = buscarNoTerminal(cadena, G)) != '\0') {
+        int indices[long];
+        int count = 0;
+        // Buscar producciones aplicables al NTActual
+        for (int i = 0; i < G->cant_producciones; i++) {
+            if (G->producciones[i].noTerminal == NTActual) {
+                indices[count++] = i;
+            }
+        }
+
+        if (count == 0) break;
+
+        int elegida = indices[rand() % count];
+        char *reemplazo = G->producciones[elegida].produccion;
+
+        int pos;
+        for (int i = 0; cadena[i] != '\0'; i++) {
+            if (cadena[i] == NTActual) {
+                pos = i;
+                break;
+            }
+        }
+
+        char nuevaCadena[2048] = "";
+        strncat(nuevaCadena, cadena, pos);     // Copia parte antes del no terminal
+        strcat(nuevaCadena, reemplazo);        // Copia la producción (reemplazo)
+        strcat(nuevaCadena, cadena + pos + 1); // Copia el resto después del no terminal
+
+        strcpy(cadena, nuevaCadena);
+    }
+
+    printf("Palabra generada: %s\n", cadena);
+}
 
 // Función para validar si una producción es regular
 int esProduccionRegular(char *ladoDerecho)
@@ -37,7 +92,7 @@ int esProduccionRegular(char *ladoDerecho)
     }
     else if (len == 2)
     {
-        return islower(ladoDerecho[0]) && isupper(ladoDerecho[1]) ; // no terminal + terminal 
+        return islower(ladoDerecho[0]) && isupper(ladoDerecho[1]) ; // terminal + no terminal 
     }
     
 
@@ -54,20 +109,33 @@ int esGramaticaRegular(struct Gramatica *G)
     return 1;
 }
 
+// Elimina espacios de Terminales y noTerminales
+void limpiarEspacios(char *dest, const char *src) {
+    int j = 0;
+    for (int i = 0; src[i] != '\0'; i++) {
+        if (src[i] != ' ') {
+            dest[j++] = src[i];
+        }
+    }
+    dest[j] = '\0';
+}
 
 // Prodecimiento para ingresar una gramatica formal
 void ingresarGramatica(struct Gramatica *G)
 {
-    printf("Ingrese los símbolos no terminales separados por un espacio: ");
-    scanf(" %[^\n]", G -> noTerminales);
+    char aux[long];
 
-    printf("Ingrese los símbolos terminales separados por un espacio: ");
-    scanf(" %[^\n]", G -> Terminales);
+    printf("Ingrese los simbolos no terminales separados por un espacio: ");
+    scanf(" %[^\n]", aux);
+    limpiarEspacios(G->noTerminales, aux);
 
+    printf("Ingrese los simbolos terminales separados por un espacio: ");
+    scanf(" %[^\n]", aux);
+    limpiarEspacios(G->Terminales, aux);
+    
     printf("Ingrese las producciones en el formato S->xT :\n");
     printf("Ingrese fin para finalizar.\n");
 
-    char aux[long];
     G->cant_producciones = 0;
     while (1)
     {
@@ -97,6 +165,8 @@ void MostrarGramatica(struct Gramatica *G)
     for (int i=0 ; G->noTerminales[i] != '\0' ; i++)
     {
         printf("%c" , G->noTerminales[i]);
+        if (G->noTerminales[i + 1] != '\0') // si no es el último
+            printf(", ");
     }
 
     printf("\n");
@@ -105,11 +175,13 @@ void MostrarGramatica(struct Gramatica *G)
     for (int i=0 ; G->Terminales[i] != '\0' ; i++)
     {
         printf("%c" , G->Terminales[i]);
+        if (G->Terminales[i + 1] != '\0') // si no es el último
+            printf(", ");
     }
 
     printf("\n");
 
-    printf("Producciones: ");
+    printf("Producciones: \n");
     for (int i=0 ; i < G->cant_producciones ; i++)
     {
         printf(" %c -> %s \n" , G->producciones[i].noTerminal,G->producciones[i].produccion);
@@ -136,6 +208,6 @@ int main()
     else
         printf("La Gramatica ingresada es regular.\n");
 
-
+    derivarGramatica (&G);
     return 0;
 }
